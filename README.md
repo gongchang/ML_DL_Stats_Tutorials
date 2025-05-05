38795bd7cf76bf569836115f78b08627df845ea4
Xingyu Li <xingyuli@mac.lan>
2025-05-04 22:21:44 -0400

    updates to XAI with conda virtual environments

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ML_DL_Stats_Tutorials
-The adoption of Machine learning (including deep learning) and advanced statistical methods can be made easier with well-documented tutorials. This repo intends to provide detailed tutorials on the use of ML/DL/Stats framworks/packages  for machine learning:
+The adoption of Machine learning (including deep learning) and advanced statistical methods can be made easier with well-documented tutorials. This repo intends to provide detailed tutorials on the use of ML/DL/Stats framworks/packages for machine learning:
 - XAI (explainable AI)
 - AutoML
 - Scikit-learn
--- automl_tutorials/automl_best_model_pipeline_by_tpot.py
+++ automl_tutorials/automl_best_model_pipeline_by_tpot.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.kernel_approximation import RBFSampler
-from sklearn.linear_model import LassoLarsCV
+from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -12,10 +13,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=1)
 
-# Average CV score on the training set was: -3129.3452341689585
+# Average CV score on the training set was: -3061.024638961212
 exported_pipeline = make_pipeline(
-    RBFSampler(gamma=0.9500000000000001),
-    LassoLarsCV(normalize=True)
+    StackingEstimator(estimator=KNeighborsRegressor(n_neighbors=41, p=1, weights="uniform")),
+    RidgeCV()
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 1)
